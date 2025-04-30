8b876b6ed19190d8652c55f1e7a1ab70548598e5
sarthak576 <sarthakguptarjn@gmail.com>
2025-04-30 15:55:14 +0530

    30 apr 2025 odd-even sum...

--- sumupto_n.java
+++ sumupto_n.java
@@ -1,10 +1,11 @@
 public class sumupto_n {
     public static void main(String[] args) {
-        System.out.println("Sum upto 26: " + sum(26));
+        System.out.println("Sum upto 101 " + sum(101));
+
 
     }
 
-    public static double sum(double k) {
+    public static int sum(int k) {
         return (k * (k + 1)) / 2;
         
     }
